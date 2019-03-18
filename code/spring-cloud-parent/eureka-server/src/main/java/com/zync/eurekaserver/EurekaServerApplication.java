04d4c136e0f34479a41321b7fa462a1ca540c913
luocong2013 <luocong2013@outlook.com>
2019-03-18 23:04:33 +0800

    Eureka集群配置

---
+++ code/spring-cloud-parent/eureka-server/src/main/java/com/zync/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.zync.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author luoc
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
