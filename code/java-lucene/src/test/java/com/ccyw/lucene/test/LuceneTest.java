7ec8cafc0a40a93ecc7ab9d64dcba18bf8dc263e
罗聪 <luocong2013@outlook.com>
2019-09-08 22:48:09 +0800

    Lucene学习

--- code/java-lucene/src/test/java/com/ccyw/lucene/test/LuceneTest.java
+++ code/java-lucene/src/test/java/com/ccyw/lucene/test/LuceneTest.java
@@ -5,6 +5,9 @@ import com.ccyw.lucene.bean.Article;
 import com.ccyw.lucene.bean.QueryResult;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author luoc
  * @version V1.0.0
@@ -14,13 +17,30 @@ import org.junit.Test;
 public class LuceneTest {
 
     @Test
-    public void testInsert() throws Exception {
-        System.out.println("*****************检索开始******************");
+    public void testBatchInsert() throws Exception {
+        System.out.println("*****************索引开始******************");
         long startTime = System.currentTimeMillis();
-        Article article = new Article(1, "Google", "成立于1998年9月4日，由拉里·佩奇和谢尔盖·布林共同创建，被公认为全球最大的搜索引擎。谷歌是一家位于美国的跨国科技企业.");
-        LuceneHelper.insert(article);
+        List<Article> articles = new ArrayList<>();
+        for (int i = 1; i <= 1000; i++) {
+            Article article = new Article(i, "title" + i, "这是测试文档" + i);
+            articles.add(article);
+        }
+        LuceneHelper.batchInsert(articles);
         long endTime = System.currentTimeMillis();
-        System.out.println("*****************检索结束******************");
+        System.out.println("*****************索引结束******************");
+        System.out.println("总共花费"+(endTime-startTime)+"ms");
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        System.out.println("*****************索引开始******************");
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= 10000; i++) {
+            Article article = new Article(i, "Google" + i, "谷歌公司（Google Inc.）成立于1998年9月4日，由拉里·佩奇和谢尔盖·布林共同创建，被公认为全球最大的搜索引擎。" + i);
+            LuceneHelper.insert(article);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("*****************索引结束******************");
         System.out.println("总共花费"+(endTime-startTime)+"ms");
     }
 
