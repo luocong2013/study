0fa1a73d6c4f54ecdb7a13bf31b90618807b677f
luocong <luocong2013@outlook.com>
2020-02-10 16:04:48 +0800

    Kafka

---
+++ code/java-kafka/src/test/java/com/zync/kafka/JavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zync.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
