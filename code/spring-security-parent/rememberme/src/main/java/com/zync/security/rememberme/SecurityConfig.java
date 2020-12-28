c57f10668b4020d6246f433d17c7247027cf7d3b
luocong <luocong2013@outlook.com>
2020-12-28 17:19:54 +0800

    Spring Security 自动登录

---
+++ code/spring-security-parent/rememberme/src/main/java/com/zync/security/rememberme/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.zync.security.rememberme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author luocong
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("javaboy")
+                .password("123").roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .rememberMe()
+                .key("javaboy")
+                .and()
+                .csrf().disable();
+    }
+}
---
+++ code/spring-security-parent/rememberme/src/test/java/com/zync/security/rememberme/RemembermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zync.security.rememberme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemembermeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
