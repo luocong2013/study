ca0e38cc29c0f3f7f5a1863ceaebf9de44e2784b
luocong <luocong2013@outlook.com>
2020-12-30 14:11:23 +0800

    Spring Security 自定义登录加入验证码功能

---
+++ code/spring-security-parent/verifycode/src/main/java/com/zync/security/verifycode/VerifycodeApplication.java
@@ -0,0 +1,16 @@
+package com.zync.security.verifycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luocong
+ */
+@SpringBootApplication
+public class VerifycodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerifycodeApplication.class, args);
+    }
+
+}
---
+++ code/spring-security-parent/verifycode/src/test/java/com/zync/security/verifycode/VerifycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zync.security.verifycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifycodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
