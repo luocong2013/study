087363c8ae78fa0b7015dd7cb707ff199ece1152
luocong <luocong2013@outlook.com>
2021-04-14 10:15:25 +0800

    Spring Cloud Alibaba Nacos Config Refresh

--- code/spring-cloud-alibaba-parent/nacos-discovery-provide/src/main/java/com/zync/nacos/nacosdiscoveryprovide/controller/ProviderController.java
+++ code/spring-cloud-alibaba-parent/nacos-discovery-provide/src/main/java/com/zync/nacos/nacosdiscoveryprovide/controller/ProviderController.java
@@ -1,5 +1,9 @@
 package com.zync.nacos.nacosdiscoveryprovide.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @description 生产者控制器
  * @date 2020/11/18 21:15
  */
+@RefreshScope
 @RestController
 public class ProviderController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${coder.username}")
+    private String username;
+
+    @GetMapping("/username")
+    public String username() {
+        return username;
+    }
+
+    @GetMapping("/username1")
+    public String username1() {
+        return environment.getProperty("coder.username");
+    }
 
     @GetMapping("/hi/{username}")
     public String hi(@PathVariable String username) {
