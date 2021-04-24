4e027703eebf24047e1a1cd74643946539aa1430
luocong <luocong2013@outlook.com>
2021-04-24 22:47:56 +0800

    更换IDEA

--- code/java-leetcode/src/main/java/com/zync/leetcode/bytedance/Interview.java
+++ code/java-leetcode/src/main/java/com/zync/leetcode/bytedance/Interview.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class Interview {
 
     public static void main(String[] args) {
-//        int[] array = {1, 2, 3, 4, 2, 1};  // 1 2 4 1 2 3
+        //int[] array = {1, 2, 3, 4, 2, 1};  // 1 2 4 1 2 3
         int[] array = {1, 2, 3, 9, 8, 4, 2, 1};
         System.out.println(Arrays.toString(temp(array)));
     }
