c25ee3d53180d1ad6434922e9971886548580152
luocong <luocong2013@outlook.com>
2022-01-26 18:10:02 +0800

    log4j2

---
+++ code/spring-boot-log/spring-boot-log4j2/src/main/java/com/zync/log4j2/SpringBootLog4j2Application.java
@@ -0,0 +1,19 @@
+package com.zync.log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * log4j2 springboot启动类
+ *
+ * @author luocong
+ * @version v1.0
+ * @date 2022/1/25 15:06
+ */
+@SpringBootApplication
+public class SpringBootLog4j2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLog4j2Application.class, args);
+    }
+}
