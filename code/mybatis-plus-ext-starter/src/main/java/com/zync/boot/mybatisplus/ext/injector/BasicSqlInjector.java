78dc73b391d1efbc7e0235431081f97df0013eb7
luocong <luocong2013@outlook.com>
2022-01-30 15:43:57 +0800

    扩展Mybatis Plus批量插入功能

---
+++ code/mybatis-plus-ext-starter/src/main/java/com/zync/boot/mybatisplus/ext/injector/BasicSqlInjector.java
@@ -0,0 +1,43 @@
+package com.zync.boot.mybatisplus.ext.injector;
+
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
+import com.zync.boot.mybatisplus.ext.annotation.IgnoreInsert;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+/**
+ * 自定义SqlInjector
+ *
+ * @author luocong
+ * @version v1.0
+ * @date 2022/1/28 10:18
+ */
+public class BasicSqlInjector extends DefaultSqlInjector {
+
+    @Override
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
+        methodList.add(new InsertBatchSomeColumn(predicate()));
+        return methodList;
+    }
+
+    /**
+     * 字段筛选条件
+     * @return Predicate
+     */
+    private Predicate<TableFieldInfo> predicate() {
+        return tableField -> {
+            Field field = tableField.getField();
+            IgnoreInsert annotation = field.getAnnotation(IgnoreInsert.class);
+            return Objects.isNull(annotation);
+        };
+    }
+
+}
