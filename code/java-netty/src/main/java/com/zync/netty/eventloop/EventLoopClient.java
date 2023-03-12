a0955d461593b569cd2d05b5752b00efd89cdcc1
luocong <luocong2013@outlook.com>
2023-03-12 21:45:54 +0800

    Netty ChannelFuture

--- code/java-netty/src/main/java/com/zync/netty/eventloop/EventLoopClient.java
+++ code/java-netty/src/main/java/com/zync/netty/eventloop/EventLoopClient.java
@@ -2,6 +2,7 @@ package com.zync.netty.eventloop;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -22,8 +23,8 @@ import java.nio.charset.StandardCharsets;
 public class EventLoopClient {
 
     public static void main(String[] args) throws InterruptedException {
-        // 1. 客户端启动器
-        Channel channel = new Bootstrap()
+        // 1. 带有 Future，Promise 的类型都是和异步方法配套使用，用来正确处理结果
+        ChannelFuture channelFuture = new Bootstrap()
                 // 2. 添加 EventLoop
                 .group(new NioEventLoopGroup())
                 // 3. 选择客户端 channel 实现
@@ -37,11 +38,25 @@ public class EventLoopClient {
                     }
                 })
                 // 5. 连接到服务器
-                .connect(new InetSocketAddress("localhost", 8610))
-                // 6. 阻塞方法，直到连接建立
-                .sync()
-                .channel();
+                // 异步非阻塞，主线程发起调用，真正执行 connect 是 NioEventLoopGroup 中的 nio 线程
+                .connect(new InetSocketAddress("localhost", 8610));
+
+        // 1.1 使用 sync 方法同步处理结果
+        // 阻塞住当前线程，直到nio线程连接建立完毕
+        channelFuture.sync();
+        Channel channel = channelFuture.channel();
         log.debug("{}", channel);
         channel.writeAndFlush("hi");
+
+        // 1.2 使用 addListener(回调对象) 方法异步处理结果
+        //channelFuture.addListener(new ChannelFutureListener() {
+        //    // 在 nio 线程连接建立好之后，会调用 operationComplete
+        //    @Override
+        //    public void operationComplete(ChannelFuture future) throws Exception {
+        //        Channel channel = future.channel();
+        //        log.debug("{}", channel);
+        //        channel.writeAndFlush("hi");
+        //    }
+        //});
     }
 }
