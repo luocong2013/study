de3394273fc8a560a921574efa4ba50c8c89a214
luocong <luocong2013@outlook.com>
2023-04-05 22:05:21 +0800

    Netty @Sharable

---
+++ code/java-netty/src/main/java/com/zync/chat/server/ChatServer.java
@@ -0,0 +1,58 @@
+package com.zync.chat.server;
+
+import com.zync.chat.protocol.MessageCodecSharable;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Chat 服务端
+ *
+ * @author luocong
+ * @version v1.0
+ * @date 2023/4/5 21:44
+ */
+@Slf4j
+public class ChatServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        LoggingHandler loggingHandler = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable messageCodec = new MessageCodecSharable();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.group(boss, worker);
+            bootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+                    pipeline.addLast(new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0));
+                    pipeline.addLast(loggingHandler);
+                    pipeline.addLast(messageCodec);
+                }
+            });
+            Channel channel = bootstrap.bind(8888).sync().channel();
+            log.debug("{} server start...", channel);
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.debug("server stop");
+        }
+    }
+}
