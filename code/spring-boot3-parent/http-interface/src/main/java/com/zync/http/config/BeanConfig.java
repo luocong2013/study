dcb4b9ab7af0a301e61efd6a214636b65d87117e
luocong <luocong2013@outlook.com>
2024-04-28 15:44:29 +0800

    springboot 3 http interface timeout

--- code/spring-boot3-parent/http-interface/src/main/java/com/zync/http/config/BeanConfig.java
+++ code/spring-boot3-parent/http-interface/src/main/java/com/zync/http/config/BeanConfig.java
@@ -1,11 +1,17 @@
 package com.zync.http.config;
 
 import com.zync.http.api.DemoApi;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 /**
  * Bean配置文件
@@ -19,8 +25,13 @@ public class BeanConfig {
 
     @Bean
     public DemoApi demoApi() {
-        WebClient client = WebClient.builder().baseUrl("http://t.weather.sojson.com/").build();
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
+                .responseTimeout(Duration.ofSeconds(3))
+                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(3)));
+
+        WebClient client = WebClient.builder().baseUrl("http://t.weather.sojson.com/").clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(client)).build();
         return factory.createClient(DemoApi.class);
     }
 
