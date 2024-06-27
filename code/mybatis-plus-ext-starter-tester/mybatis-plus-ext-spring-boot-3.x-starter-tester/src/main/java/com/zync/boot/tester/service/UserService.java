0791fadcfd1632dd8f6c2709b20bcb7f75b08ad3
luocong <luocong2013@outlook.com>
2024-06-27 18:08:56 +0800

    扩展Mybatis Plus功能的SpringBoot 3.x Starter Tester

---
+++ code/mybatis-plus-ext-starter-tester/mybatis-plus-ext-spring-boot-3.x-starter-tester/src/main/java/com/zync/boot/tester/service/UserService.java
@@ -0,0 +1,16 @@
+package com.zync.boot.tester.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zync.boot.tester.entity.User;
+
+/**
+ * <p>
+ * 用户表 服务接口
+ * </p>
+ *
+ * @author luocong
+ * @since 2022-01-30
+ */
+public interface UserService extends IService<User> {
+
+}
