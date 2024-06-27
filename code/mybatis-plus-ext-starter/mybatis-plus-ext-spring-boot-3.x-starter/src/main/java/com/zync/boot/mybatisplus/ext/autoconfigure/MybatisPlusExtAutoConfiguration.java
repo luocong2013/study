5adf95aa6a32e5239f97aa9078021f38eda3b4ab
luocong <luocong2013@outlook.com>
2024-06-27 18:04:43 +0800

    扩展Mybatis Plus功能的SpringBoot 3.x Starter

---
+++ code/mybatis-plus-ext-starter/mybatis-plus-ext-spring-boot-3.x-starter/src/main/java/com/zync/boot/mybatisplus/ext/autoconfigure/MybatisPlusExtAutoConfiguration.java
@@ -0,0 +1,36 @@
+package com.zync.boot.mybatisplus.ext.autoconfigure;
+
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.zync.boot.mybatisplus.ext.core.handler.DefaultMetaObjectHandler;
+import com.zync.boot.mybatisplus.ext.core.injector.BasicSqlInjector;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis plus ext 自动配置类
+ *
+ * @author luocong
+ * @version v1.0
+ * @date 2022/1/28 17:15
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({MybatisPlusAutoConfiguration.class})
+@AutoConfigureBefore({MybatisPlusAutoConfiguration.class})
+public class MybatisPlusExtAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public BasicSqlInjector basicSqlInjector() {
+        return new BasicSqlInjector();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DefaultMetaObjectHandler defaultMetaObjectHandler() {
+        return new DefaultMetaObjectHandler();
+    }
+
+}
