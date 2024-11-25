d3edabe3b428168cd3272b6efa0663e7918f2303
luocong <luocong2013@outlook.com>
2024-11-25 18:04:47 +0800

    Spring Boot 3.4 结构化日志记录

---
+++ code/spring-boot3-parent/logback-log-format/src/main/java/com/zync/log/format/Application.java
@@ -0,0 +1,19 @@
+package com.zync.log.format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author luocong
+ * @version v1.0
+ * @date 2024/11/25 16:37
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
